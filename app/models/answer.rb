dadf0205edc7c21b777f67d63ba57378d86a6f12
v-kazantsev <vvs201@mail.ru>
2018-01-12 12:52:56 +1000

    Работа над ошибками

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,8 @@ class Answer < ApplicationRecord
   validates_presence_of :body
   belongs_to :question
   scope :correct, -> { where(correct: true) }
+  validate :too_many
+  def too_many
+    errors.add(:question, 'Too many answers') if question.answers.count > 4
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,5 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
   has_many :tests
   validates_presence_of :email
-
-  def get_tests_by_level(level)
-    self.tests.where(level: level)
-  end
+  
 end
